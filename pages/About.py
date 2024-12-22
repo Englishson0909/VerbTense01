eaf8b69c28fae820f629b3a8da8b9e9d264dbe19
EnglishSon <127401703+Englishson0909@users.noreply.github.com>
2024-12-22 23:42:57 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,3 +1,14 @@
 import streamlit as st
 st.title ("About Irregular Verbs-24")
-st.write ("To learn irregular Verbs")
+st.write (https://github.com/Englishson0909/VerbTense01/raw/main/depositphotos_196778222-stock-illustration-smiley-thumb-laik-cool-emoticon.jpg)
+("This app will help you easily learn the three forms of verbs (present, past, and past participle)
+
+About This App
+
+This app was created to help learners master the three forms of verbs (present, past, and past participle).
+
+Creator:
+The app was developed by Hansuk Son. 
+
+Verbs Included:
+This app contains 100 irregular verbs. These verbs are selected from the list of words provided by the Korean Institute for Curriculum and Evaluation (KICE), ensuring they are aligned with the national educational standards.")
