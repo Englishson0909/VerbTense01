396a97bd3315a517a3b78eb8f8b9606f47a9c5f4
EnglishSon <127401703+Englishson0909@users.noreply.github.com>
2024-12-22 23:23:14 +0900

    Home.py

--- Home.py
+++ Home.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
 st.write (""" Welcome to my classroom, Hi, my name is Hansuk. Since 2004, I’ve been teaching English, and although there have been many challenges, I believe it’s crucial to keep moving forward and grow as an educator every day.
-
 As an English teacher, I’d like to highlight some key goals for learning English:
-
 Learning English as an EFL student can open up an entirely new world of possibilities.
 It allows learners to discover different perspectives and cultures.
 It provides access to a wealth of resources, ideas, information, and networks.
 It expands career options in the global job market.
 Finally, it boosts confidence and fosters collaboration and communication across diverse cultures.
-I look forward to guiding my students toward these goals, confident that each step forward prepares them for a brighter future.""" )
-[https://flexible.img.hani.co.kr/flexible/normal/970/582/imgdb/child/2024/1010/53_17285251176767_20241010501060.jpg]
+I look forward to guiding my students toward these goals, confident that each step forward prepares them for a brighter future.
+To have another language is to possess a second soul
+[https://flexible.img.hani.co.kr/flexible/normal/970/582/imgdb/child/2024/1010/53_17285251176767_20241010501060.jpg]""")
