2ad41c4ba91bd118f553f743fbd4d649f3111442
EnglishSon <127401703+Englishson0909@users.noreply.github.com>
2024-12-22 23:35:51 +0900

    Update About.py

--- About.py
+++ About.py
@@ -1,2 +1,12 @@
 
 This app will help you easily learn the three forms of verbs (present, past, and past participle)
+
+About This App
+
+This app was created to help learners master the three forms of verbs (present, past, and past participle).
+
+Creator:
+The app was developed by Hansuk Son. 
+
+Verbs Included:
+This app contains 100 irregular verbs. These verbs are selected from the list of words provided by the Korean Institute for Curriculum and Evaluation (KICE), ensuring they are aligned with the national educational standards.
