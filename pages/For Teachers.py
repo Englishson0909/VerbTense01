c1ec89e487a96aa0bda359ff8a9ad71a2fc79e50
EnglishSon <127401703+Englishson0909@users.noreply.github.com>
2024-12-27 00:18:58 +0900

    Update For Teachers.py

--- pages/For Teachers.py
+++ pages/For Teachers.py
@@ -5,13 +5,16 @@ def main():
     st.markdown(
         """
         This app is designed to help students systematically practice the three principal parts of English verbs 
-        (present, past, and past participle). 
-        It focuses on 100 key irregular verbs—selected from the core vocabulary recommended by the **Korea Institute for Curriculum and Evaluation (KICE)** 
-        and other education authorities—and encourages learners to input the past and past participle forms on their own for hands-on practice. 
+        (present, past, and past participle).  
+        It focuses on 100 key irregular verbs—selected from the core vocabulary recommended by the **Korea Institute 
+        for Curriculum and Evaluation (KICE)** and other education authorities—and encourages learners to input the 
+        past and past participle forms on their own for hands-on practice.  
         
-        Additionally, the app provides audio so students can listen to each verb’s pronunciation and become more familiar with these words. 
+        Additionally, the app provides audio so students can listen to each verb’s pronunciation and become more 
+        familiar with these words.  
         Overall, the goal is to guide students in naturally mastering irregular verb forms and enhancing their language proficiency.
+
+        **If you want to know the words list, [click here (The word list)](https://github.com/Hansukson/Application2/raw/main/irregular_verbs%20(1).csv).**
         """
     )
 
-    
