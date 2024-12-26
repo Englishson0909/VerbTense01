a609807033a2ba7e1482424b251b49e41a3eec7f
EnglishSon <127401703+Englishson0909@users.noreply.github.com>
2024-12-27 00:34:28 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,28 +1,31 @@
 import streamlit as st
 
-st.write("""
+st.write("""  To have another language is to possess a second soul. 
+
     Welcome to my classroom, Hi, my name is Hansuk. Since 2004, I’ve been teaching English, and although there have been many challenges, I believe it’s crucial to keep moving forward and grow as an educator every day.
     As an English teacher, I’d like to highlight some key goals for learning English:
     
     Learning English as an EFL student can open up an entirely new world of possibilities.
     
-    It allows learners to discover different perspectives and cultures.
+    - It allows learners to discover different perspectives and cultures.
     
-    It provides access to a wealth of resources, ideas, information, and networks.
+    - It provides access to a wealth of resources, ideas, information, and networks.
     
-    It expands career options in the global job market.
+    - It expands career options in the global job market.
     
-    Finally, it boosts confidence and fosters collaboration and communication across diverse cultures.
+    - Finally, it boosts confidence and fosters collaboration and communication across diverse cultures.
     I look forward to guiding my students toward these goals, confident that each step forward prepares them for a brighter future.
 
 Key Features of the App:
 
 A brief list or overview of what users can do on the app (e.g., learning irregular verbs, mastering verb tenses, etc.).
 Example: "Here, you can practice irregular verbs, learn verb conjugations, and improve your English skills through engaging exercises."
-    
-    To have another language is to possess a second soul
-""")
+      """)
+
+
 
 # Display image
 st.image("https://flexible.img.hani.co.kr/flexible/normal/970/582/imgdb/child/2024/1010/53_17285251176767_20241010501060.jpg")
-
+st.write("""The three pine trees on Geogeum Island, planted over a hundred years ago, stand strong through changing tides, showing real perseverance.
+They stay firm even under the sea, reminding us that our hopes can remain steady.
+Learning a foreign language is like quietly getting through each day, and the calm beaches and camel-shaped shore remind us that our power to endure lies within.""")
