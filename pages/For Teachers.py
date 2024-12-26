6df01079ae3c2ca5d7f61bbe5a9b85c16a8340ca
EnglishSon <127401703+Englishson0909@users.noreply.github.com>
2024-12-26 23:58:41 +0900

    Create For Teachers.py

---
+++ pages/For Teachers.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def main():
+    st.title("For Teachers")
+    st.markdown(
+        """
+        This app is designed to help students systematically practice the three principal parts of English verbs 
+        (present, past, and past participle). 
+        It focuses on 100 key irregular verbs—selected from the core vocabulary recommended by the **Korea Institute for Curriculum and Evaluation (KICE)** 
+        and other education authorities—and encourages learners to input the past and past participle forms on their own for hands-on practice. 
+        
+        Additionally, the app provides audio so students can listen to each verb’s pronunciation and become more familiar with these words. 
+        Overall, the goal is to guide students in naturally mastering irregular verb forms and enhancing their language proficiency.
+        """
+    )
+
+    
